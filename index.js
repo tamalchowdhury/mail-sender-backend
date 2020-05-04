fe6ca89f2010a2f15c0daf94ad89bb358234a94c
Tamal Anwar <tamalanwaronline@gmail.com>
2020-05-04 19:52:06 +0600

    helmet js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const messages = require('./messages');
 const EmailService = require('./services/EmailService');
 
 const FROM_EMAIL = process.env.FROM_EMAIL;
 const PORT = process.env.PORT || 5000;
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
